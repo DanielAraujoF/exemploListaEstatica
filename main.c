77b3620951c49ee486eb18b1ed05e2f93c87051c
Daniel Araujo <danielaraujo.github@gmail.com>
2023-08-01 20:44:43 -0300

    Commit message

--- main.c
+++ main.c
@@ -12,14 +12,36 @@
 //     return num;
 // }
 
-// só é possível acessar o num[] até onde eu criei em "int num[5];", no caso um "num[5] = 4;" não funciona.
+//  só é possível acessar o num[] até onde eu criei em "int num[5];", no caso um "num[5] = 4;" não funciona.
+// int main() {
+//     int num[5];
+//     num[0] = 1;
+//     num[1] = 2;
+//     num[2] = 3;
+//     num[3] = 4;
+//     num[4] = 5;
+//
+//     return 0;
+// }
+
 int main() {
     int num[5];
-    num[0] = 1;
-    num[1] = 2;
-    num[2] = 3;
-    num[3] = 4;
-    num[4] = 5;
+    int i;
+    // for (valor de início; condição de continuidade; incremento, caso continuidade)
+    for (i = 0; i < 5; i++) {
+        printf("Informe o valor da posição %d: ", i);
+        scanf("%d", &num[i]);
+    }
+
+    for (i = 0; i < 5; i++) {
+        printf("O valor informado na posição %d foi %d\n", i, num[i]);
+    }
 
     return 0;
 }
+
+// o tipo do main tem q ser correspondente com o formatador ↓
+// int → %d
+// char → %s
+// float → %f
+// double → %lf
